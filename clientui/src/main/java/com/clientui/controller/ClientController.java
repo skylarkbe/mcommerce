6b6b5f927250eb4b50a6bfcc54e675ca40ad46e5
romain.vanderkeilen <t4MJeDc7yJjKinhwhvcJ>
2018-12-06 13:53:14 +0100

    Full client with micro services

--- clientui/src/main/java/com/clientui/controller/ClientController.java
+++ clientui/src/main/java/com/clientui/controller/ClientController.java
@@ -1,22 +1,36 @@
 package com.clientui.controller;
 
+import com.clientui.beans.CommandeBean;
+import com.clientui.beans.PaiementBean;
 import com.clientui.beans.ProductBean;
+import com.clientui.proxies.MicroserviceCommandeProxy;
+import com.clientui.proxies.MicroservicePaiementProxy;
 import com.clientui.proxies.MicroserviceProduitsProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Controller
 public class ClientController {
 
     @Autowired
     private MicroserviceProduitsProxy produitsProxy ;
+
+    @Autowired
+    private MicroserviceCommandeProxy commandesProxy;
+
+    @Autowired
+    private MicroservicePaiementProxy paiementProxy;
 
     @RequestMapping("/")
     public String accueil(Model model) {
@@ -32,6 +46,53 @@ public class ClientController {
             model.addAttribute("produit", product.get());
         }
         return "FicheProduit" ;
+    }
+
+    @RequestMapping(value = "/commander-produit/{idProduit}/{montant}")
+    public String passerCommande(@PathVariable int idProduit, @PathVariable Double montant,  Model model){
+        CommandeBean commande = new CommandeBean();
+
+        //On renseigne les propriétés de l'objet de type CommandeBean que nous avons crée
+        commande.setProductId(idProduit);
+        commande.setQuantite(1);
+        commande.setDateCommande(new Date());
+
+        //appel du microservice commandes grâce à Feign et on récupère en retour les détails de la commande créée, notamment son ID (étape 4).
+        CommandeBean commandeAjoutee = commandesProxy.ajouterCommande(commande);
+
+        //on passe à la vue l'objet commande et le montant de celle-ci afin d'avoir les informations nécessaire pour le paiement
+        model.addAttribute("commande", commandeAjoutee);
+        model.addAttribute("montant", montant);
+
+        return "Paiement";
+    }
+
+    @RequestMapping(value = "/payer-commande/{idCommande}/{montantCommande}")
+    public String payerCommande(@PathVariable int idCommande, @PathVariable Double montantCommande, Model model){
+        PaiementBean paiementAExcecuter = new PaiementBean();
+
+        //on reseigne les détails du produit
+        paiementAExcecuter.setIdCommande(idCommande);
+        paiementAExcecuter.setMontant(montantCommande);
+        paiementAExcecuter.setNumeroCarte(numcarte()); // on génère un numéro au hasard pour simuler une CB
+
+        // On appel le microservice et (étape 7) on récupère le résultat qui est sous forme ResponseEntity<PaiementBean> ce qui va nous permettre de vérifier le code retour.
+        ResponseEntity<PaiementBean> paiement = paiementProxy.payerUneCommande(paiementAExcecuter);
+
+        Boolean paiementAccepte = false;
+        //si le code est autre que 201 CREATED, c'est que le paiement n'a pas pu aboutir.
+        if(paiement.getStatusCode() == HttpStatus.CREATED)
+            paiementAccepte = true;
+
+        model.addAttribute("paiementOk", paiementAccepte); // on envoie un Boolean paiementOk à la vue
+        model.addAttribute("idCommande", idCommande); // on envoie également le numéro de commande pour l'afficher
+
+        return "Confirmation";
+    }
+
+    //Génére une serie de 16 chiffres au hasard pour simuler vaguement une CB
+    private Long numcarte() {
+        return ThreadLocalRandom.current().nextLong(1000000000000000L,9000000000000000L );
     }
 
 }
